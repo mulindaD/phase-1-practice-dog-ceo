6e65011f2cce9b6b89675895d486f71787f30d67
Dominic Mulinda <mulindarweshumba@gmail.com>
2024-07-23 18:42:18 +0300

    TCW Update CSS and fetching dogs from API Fetching dogs from API Updated CSS Styling on the site

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+function fetchDogs() {
+    return fetch(imgUrl)
+            .then(res => res.json())
+            .then(imgs => {
+                console.log(imgs)
+                imgs.message.forEach(img => {
+                    const allDogs = document.getElementById('dog-image-container')
+                    const dogElement = document.createElement('img')
+                    dogElement.src = img
+                    allDogs.appendChild(dogElement)
+                })
+            .catch(err => console.error('Error Message: ',err.message))
+            })
+}
+
+
+document.addEventListener('DOMContentLoaded', fetchDogs)
